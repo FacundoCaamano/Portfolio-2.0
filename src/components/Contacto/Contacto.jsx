3d153bad32f15693329b8898ac896ab5a4f2d6a3
Facundo Caamaño <facundoadriancaamano@outlook.com>
2022-11-12 21:57:09 -0300

    estilos + formularios

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,9 +1,35 @@
-export const Contacto = ()=>{
-    return(
+export const Contacto = () => {
+    return (
         <div>
-            <form action="">
-                <input type="text" />
-            </form>
+            <section>
+                <h1 className="titulo__contacto">Contactame</h1>
+                <form class="formulario" action="https://formsubmit.co/facundoadriancaamano@gmail.com" method="POST">
+                    <ul class="formulario__lista">
+                        <li class="formulario__lista--item">
+                            <label for="Nombre">Nombre:</label>
+                            <input type="text" name="name" placeholder="Escriba su nombre" required />
+                        </li>
+                        <li class="formulario__lista--item">
+                            <label for="email">E-mail:</label>
+                            <input type="email" name="email"  placeholder="Escriba su e-mail" required />
+                        </li>
+                        <li class="formulario__lista--item">
+                            <label for="subject">Asunto</label>
+                            <input type="text" name="subject"  placeholder="Asunto" required />
+                        </li>
+                        <li class="formulario__lista--item">
+                            <label for="comments">Comentario</label>
+                            <textarea name="comments" cols="30" rows="10"></textarea>
+                        </li>
+                        <li class="formulario__lista--item">
+                            <input className="formulario__lista--item-btn" type="submit" value="Enviar" />
+                            <input className="formulario__lista--item-btn" type="reset" value="Limpiar formulario" />
+                        </li>
+                    </ul>
+                    <input type='hidden' name='_next' value='https://portfolio-2-0-mu.vercel.app/Contacto' />
+                    <input type='hidden' name='_captcha' value='false' />
+                </form>
+            </section>
         </div>
     )
 }
