98f2a6d143539c17bc50204c03e238ea5c3cf34c
Facundo Caamaño <facundoadriancaamano@outlook.com>
2022-11-15 17:06:16 -0300

    sweet alert

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,37 +1,40 @@
-
+import Swal from 'sweetalert2';
 import emailjs from '@emailjs/browser';
 
 export const ContactUs = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    
     emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID , e.target, process.env.REACT_APP_PUBLIC_KEY)
-      .then((result) => {
-        alert('mensaje enviado', result.text);
-      }, (error) => {
-        alert(error.text);
-      });
-  };
-
+    .then(
+      Swal.fire(
+        'Su correo fue enviado!',
+        `respondere lo mas pronto posible a la direccion ${e.target[2].value}`,
+        'success'
+        )
+        )
+        e.target.reset()
+      };
+      
   return (
     <form className='formulario animate__animated animate__bounceInUp' onSubmit={sendEmail}>
       <ul className='formulario__lista'>
         <li className='formulario__lista--item'>
           <label>Name</label>
-          <input type="text" name="user_name" />
+          <input type="text" name="user_name" required/>
         </li>
         <li className='formulario__lista--item'>
           <label>Asunto</label>
-          <input type="text" name="asunto" />
+          <input type="text" name="asunto" required/>
         </li>
         <li className='formulario__lista--item'>
           <label>Email</label>
-          <input type="email" name="user_email" />
+          <input type="email" name="user_email" required/>
         </li>
         <li className='formulario__lista--item'>
           <label>Mensaje</label>
-          <textarea name="message" />
+          <textarea name="message" required/>
         </li>
         <li className='formulario__lista--item'>
           <input type="submit" value="Enviar" />
