73382d86e83da764cd007530b1d2974905e7fe70
Facundo Caamaño <facundoadriancaamano@outlook.com>
2023-01-25 19:43:04 -0300

    nuevo proyecto

--- src/components/baseDeDatos/baseDeDatos.jsx
+++ src/components/baseDeDatos/baseDeDatos.jsx
@@ -2,10 +2,12 @@ import BebidasCaamano from '../../assets/img/imgProyectos/bebidasCaamano.png'
 import Portafolio from '../../assets/img/imgProyectos/portafolio.png'
 import VehiculosCaamaño from '../../assets/img/imgProyectos/vehiculosCaamano.png'
 import PerifericosCaamaño from '../../assets/img/imgProyectos/perifericos-caamaño.jpg'
+import tateti from '../../assets/img/imgProyectos/tateti.jpg'
 
 
 export const proyectos = [
-    {id:4 ,nombre:"Perifericos-Caamaño", pictureUrl: PerifericosCaamaño, repositorio:'https://github.com/FacundoCaamano/perifericos-caamano.git' ,link:"https://perifericos-caamano.vercel.app/", herramientas:"-html-css-ReactJs-reactboostrap-git-sweetAlert" } ,
+    {id:5 ,nombre:"Ta Te Ti", pictureUrl: tateti, repositorio:'https://github.com/FacundoCaamano/ta-te-ti.git' ,link:"https://ta-te-ti-sand.vercel.app/", herramientas:"-Vite-ReactJs-css-git" } ,
+    {id:4 ,nombre:"Perifericos-Caamaño", pictureUrl: PerifericosCaamaño, repositorio:'https://github.com/FacundoCaamano/perifericos-caamano.git' ,link:"https://perifericos-caamano.vercel.app/", herramientas:"-ReactJs-reactboostrap-git-sweetAlert" } ,
     {id:3 ,nombre:"bebidas-caamaño", pictureUrl: BebidasCaamano, repositorio:'https://github.com/FacundoCaamano/bebidasCaama-o.git' ,link:"https://bebidas-caama-fp7rd35wy-facundocaamano.vercel.app/", herramientas:"-html-css-javascript-boostrap-git-sweetAlert" } ,
     {id:2 ,nombre:"Portfolio", pictureUrl:Portafolio, repositorio:'https://github.com/FacundoCaamano/Portafolio' ,link:"https://portafolio-nine-omega.vercel.app/", herramientas:"html-scss-boostrap-git"},
     {id:1 ,nombre:"vehiculos-caamaño", pictureUrl:VehiculosCaamaño, repositorio:'https://github.com/FacundoCaamano/vehiculos-caamano.git' ,link:"https://vehiculos-caamano-74gu7bgcd-facundocaamano.vercel.app/",herramientas:"html-scss-boostrap-git"}
